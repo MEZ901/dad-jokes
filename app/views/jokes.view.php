258281d01072e9a22e227a1cf7d14806372c79be
issam <issammez44@gmail.com>
2023-01-06 23:07:11 +0100

    the static version of jokes view

---
+++ app/views/jokes.view.php
@@ -0,0 +1,15 @@
+<header class="flex justify-between m-5">
+    <h1 class="text-white text-2xl font-bold">Jokes community</h1>
+    <button type="button" class="focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-gray-800 text-white border-gray-600 hover:border-gray-400 focus:ring-gray-700">Add Joke</button>
+</header>
+<section class="m-5">
+    <div class="text-center w-100 p-6 border rounded-lg shadow-md bg-gray-800 border-gray-700 m-auto mb-5">
+        <blockquote class="text-xl italic font-semibold text-white">
+            <p>"Flowbite is just awesome. It contains tons of predesigned components and pages starting from login screen to complex dashboard. Perfect choice for your next SaaS application."</p>
+        </blockquote>
+        <div class="action hidden">
+            <button type="button" class="focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mt-3 bg-gray-600 text-white border-gray-600 hover:border-gray-400 focus:ring-gray-700">edit</button>
+            <button type="button" class="focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mt-3 bg-gray-600 text-white border-gray-600 hover:border-gray-400 focus:ring-gray-700">delete</button>
+        </div>
+    </div>
+</section>
