b603cbc9e1f83824e59decdb47769a519a60956c
issam <issammez44@gmail.com>
2023-01-07 19:29:16 +0100

    remove the errors alert to fix it later

--- app/core/Model.php
+++ app/core/Model.php
@@ -7,7 +7,7 @@ trait Model  {
     protected $offset       = 0;
     protected $order_type   = "desc";
     protected $order_column = "id";
-    public $errors           = [];
+    public $errors          = [];
 
     public function getAll(){
         $query = "select * from $this->table order by $this->order_column $this->order_type";
--- app/views/jokes.view.php
+++ app/views/jokes.view.php
@@ -1,23 +1,10 @@
 <header class="flex justify-between m-5">
-    <h1 class="text-white text-2xl font-bold">Jokes community</h1>
+    <h1 class="text-white text-2xl font-bold cursor-pointer" onclick="window.location.href='<?=ROOT?>/home'">Jokes community</h1>
     <button id="addJoke" data-modal-toggle="defaultModal" type="button" class="flex focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-gray-800 text-white border-gray-600 hover:border-gray-400 focus:ring-gray-700">
         <svg class="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>    
         Add Joke
     </button>
 </header>
-
-<?php if(!empty($errors)): ?>
-<div id="alert-border-2" class="flex p-4 mb-4 mx-5 text-red-700 bg-red-100 border-t-4 border-red-500 dark:text-red-400 dark:bg-gray-800" role="alert">
-    <svg class="flex-shrink-0 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
-    <div class="ml-3 text-sm font-medium">
-        <?= implode("<br>", $errors) ?>
-    </div>
-    <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-red-100 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex h-8 w-8 dark:bg-gray-800 dark:text-red-300 dark:hover:bg-gray-700"  data-dismiss-target="#alert-border-2" aria-label="Close">
-      <span class="sr-only">Dismiss</span>
-      <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-    </button>
-</div>
-<?php endif; ?>
 
 <section class="m-5">
     <div class="text-center w-100 p-6 border rounded-lg shadow-md bg-gray-800 border-gray-700 m-auto mb-5">
