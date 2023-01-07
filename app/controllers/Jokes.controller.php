ecdb80e0c0546f431c3aced8f87a767387ec1c5c
issam <issammez44@gmail.com>
2023-01-07 19:43:31 +0100

    show the jokes

--- app/controllers/Jokes.controller.php
+++ app/controllers/Jokes.controller.php
@@ -3,17 +3,35 @@
 class Jokes {
     use Controller;
     public function index(){
-
         $joke = new Joke;
 
+        /** insert the joke **/
         if($joke->validate($_POST)){
             $joke->insert($_POST);
             redirect('jokes');
         }
-
         $data['errors'] = $joke->errors;
-
+        
+        /** view page **/
         $this->view('jokes', $data);
+
+        /** show the jokes **/
+        $result = $joke->getAll();
+        foreach($result as $row){
+            echo
+                '<section class="m-5">
+                <div class="text-center w-100 p-6 border rounded-lg shadow-md bg-gray-800 border-gray-700 m-auto mb-5">
+                    <blockquote class="text-xl italic font-semibold text-white">
+                        <p>'. $row["joke"] .'</p>
+                    </blockquote>
+                    <div class="action hidden">
+                        <button type="button" class="focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mt-3 bg-gray-600 text-white border-gray-600 hover:border-gray-400 focus:ring-gray-700">edit</button>
+                        <button type="button" class="focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mt-3 bg-gray-600 text-white border-gray-600 hover:border-gray-400 focus:ring-gray-700">delete</button>
+                    </div>
+                </div>
+            </section>';
+        }
+        
     }
 }
 
--- app/views/jokes.view.php
+++ app/views/jokes.view.php
@@ -6,7 +6,7 @@
     </button>
 </header>
 
-<section class="m-5">
+<!-- <section class="m-5">
     <div class="text-center w-100 p-6 border rounded-lg shadow-md bg-gray-800 border-gray-700 m-auto mb-5">
         <blockquote class="text-xl italic font-semibold text-white">
             <p>"Flowbite is just awesome. It contains tons of predesigned components and pages starting from login screen to complex dashboard. Perfect choice for your next SaaS application."</p>
@@ -16,7 +16,7 @@
             <button type="button" class="focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mt-3 bg-gray-600 text-white border-gray-600 hover:border-gray-400 focus:ring-gray-700">delete</button>
         </div>
     </div>
-</section>
+</section> -->
 
 <div id="defaultModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
     <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
